cf20d41bc21d6801e9155e89777f7d0fe48afa7e
Abduhakimov <abdushukur>
2022-11-01 17:08:38 +0500

    qubti

---
+++ main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+const buttonDrop = document.querySelector('buttons-drop')
+const buttonMenu = document.querySelector('btn-menu')
+
+buttonDrop.addEventListener('click', Function(),{
+  buttonMenu,classList,drop('.btn-menu')
+})
